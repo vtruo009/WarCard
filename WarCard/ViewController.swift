74fa1a88a839e8af7b622555c2a32c5ed4f07ed8
vtruo009 <43657607+vtruo009@users.noreply.github.com>
2019-12-26 00:46:59 -0800

    Finished tutorial

--- WarCard/ViewController.swift
+++ WarCard/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightScoreLabel: UILabel!
     
+    var leftScore = 0
+    var rightScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +28,27 @@ class ViewController: UIViewController {
 
     
     @IBAction func DealTapped(_ sender: Any) {
+        //created brand new UIImage object and assigning to image porperty of UIImageView
+        //UIImage() is an empty object
+        //general way to change image
+//        leftImageView.image = UIImage(named: "card5")
+//        rightImageView.image = UIImage(named: "card14")
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
         
-        print("deal tapped")
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
         
+        if leftNumber > rightNumber {
+            //player wins
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+            //CPU wins
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
     }
     
 
