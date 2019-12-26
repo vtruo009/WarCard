9e059b3d6ba29bb383d33a66d2adf39610a83703
vtruo009 <43657607+vtruo009@users.noreply.github.com>
2019-12-25 22:43:21 -0800

    Intro to IBOutlet and IBActions

--- WarCard/ViewController.swift
+++ WarCard/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func DealTapped(_ sender: Any) {
+        
+        print("deal tapped")
+        
+    }
+    
 
 }
 
